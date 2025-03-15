a500fb54b242792ef6705415da876372ce42655f
seantzyy <149601777+seantzyy@users.noreply.github.com>
2025-03-15 10:14:02 +0800

    Update Register.java

--- src/View/Register.java
+++ src/View/Register.java
@@ -1,27 +1,33 @@
 
 package View;
 
+import Controller.Security;
+
 public class Register extends javax.swing.JPanel {
 
     public Frame frame;
-    
+    public Security security = new Security();
+
     public Register() {
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         registerBtn = new javax.swing.JButton();
-        passwordFld = new javax.swing.JTextField();
+        passwordFld = new javax.swing.JPasswordField();
         usernameFld = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        confpassFld = new javax.swing.JTextField();
+        confpassFld = new javax.swing.JPasswordField();
         backBtn = new javax.swing.JButton();
+        errorText = new javax.swing.JLabel();
 
         registerBtn.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         registerBtn.setText("REGISTER");
+        registerBtn.setAlignmentX(0.5F);
+        registerBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         registerBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 registerBtnActionPerformed(evt);
@@ -30,7 +36,7 @@ public class Register extends javax.swing.JPanel {
 
         passwordFld.setBackground(new java.awt.Color(240, 240, 240));
         passwordFld.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        passwordFld.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        passwordFld.setHorizontalAlignment(javax.swing.JPasswordField.CENTER);
         passwordFld.setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true), "PASSWORD", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 12))); // NOI18N
 
         usernameFld.setBackground(new java.awt.Color(240, 240, 240));
@@ -45,7 +51,7 @@ public class Register extends javax.swing.JPanel {
 
         confpassFld.setBackground(new java.awt.Color(240, 240, 240));
         confpassFld.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        confpassFld.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        confpassFld.setHorizontalAlignment(javax.swing.JPasswordField.CENTER);
         confpassFld.setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true), "CONFIRM PASSWORD", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 12))); // NOI18N
 
         backBtn.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
@@ -56,25 +62,30 @@ public class Register extends javax.swing.JPanel {
             }
         });
 
+        errorText.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        errorText.setForeground(new java.awt.Color(153, 0, 0));
+        errorText.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(200, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(200, 200, 200)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(errorText, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(usernameFld)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(passwordFld, javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(confpassFld, javax.swing.GroupLayout.Alignment.LEADING))
-                .addContainerGap(200, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(registerBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(200, 200, 200))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(backBtn)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(registerBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -90,28 +101,80 @@ public class Register extends javax.swing.JPanel {
                 .addComponent(passwordFld, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(confpassFld, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(errorText)
                 .addGap(18, 18, 18)
                 .addComponent(registerBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(64, Short.MAX_VALUE))
+                .addContainerGap(52, Short.MAX_VALUE))
         );
-    }// </editor-fold>//GEN-END:initComponents
 
-    private void registerBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registerBtnActionPerformed
-        frame.registerAction(usernameFld.getText(), passwordFld.getText(), confpassFld.getText());
+        errorText.getAccessibleContext().setAccessibleName("errorText");
+    }// </editor-fold>                        
+
+    private void registerBtnActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // Checks password
+        Security checker = new Security();
+        
+        String password = passwordFld.getText();
+        String confpass = confpassFld.getText();
+        String username = usernameFld.getText();
+        
+        if(password.isEmpty() || username.isEmpty() || confpass.isEmpty())
+            errorText.setText("Error: Field is empty");
+        else {
+            if(checker.checkUsername(username)) 
+            
+                if(password.equals(confpass)) {
+
+                    if(!checker.checkUser(username)){
+
+                        if(checker.checkPasswordStrength(password, errorText, passwordFld, confpassFld)) {
+                            frame.registerAction(username, password, confpass);
+                            frame.loginNav(); 
+
+                            usernameFld.setText("");
+                            passwordFld.setText("");
+                            confpassFld.setText("");
+                        }
+
+                    } else {
+                        usernameFld.setText("");
+                        passwordFld.setText("");
+                        confpassFld.setText("");
+                        errorText.setText("Error: Username already exist");
+                    }
+
+                } else {
+                    passwordFld.setText("");
+                    confpassFld.setText("");
+                    errorText.setText("Error: Confirmation Password is not Similar");
+                }
+            
+            else {
+                usernameFld.setText("");
+                passwordFld.setText("");
+                confpassFld.setText("");
+                errorText.setText("Error: Username cannot be greater than 10 characters");
+            }
+            
+        }
+    }                                           
+
+    private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {                                        
+        usernameFld.setText("");
+        passwordFld.setText("");
+        confpassFld.setText("");
         frame.loginNav();
-    }//GEN-LAST:event_registerBtnActionPerformed
-
-    private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBtnActionPerformed
-        frame.loginNav();
-    }//GEN-LAST:event_backBtnActionPerformed
+    }                                       
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JButton backBtn;
     private javax.swing.JTextField confpassFld;
+    private javax.swing.JLabel errorText;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField passwordFld;
     private javax.swing.JButton registerBtn;
     private javax.swing.JTextField usernameFld;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
